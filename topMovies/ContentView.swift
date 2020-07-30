4930bda15413e3ee13dd86e4c6d55176ecbf738e
riiid <jiyeon.song@riiid.co>
2020-07-30 15:27:28 +0900

    first commit

---
+++ topMovies/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  topMovies
+//
+//  Created by riiid on 2020/07/30.
+//  Copyright © 2020 songzi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("🐙")
+            }
+        }
+        .navigationBarTitle("Octopus")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
